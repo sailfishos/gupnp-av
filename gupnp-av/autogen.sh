18748f6c673479efc5315ff0d436ff1f346bebeb
Islam Amer <islam.amer@jollamobile.com>
2014-03-16 23:23:53 +0200

    Disable gtk-doc

--- gupnp-av/autogen.sh
+++ gupnp-av/autogen.sh
@@ -24,12 +24,6 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd "$srcdir"
 
-GTKDOCIZE=`which gtkdocize`
-if test -z $GTKDOCIZE; then
-        echo "*** No GTK-Doc found, please install it ***"
-        exit 1
-fi
-
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
         echo "*** No autoreconf found, please install it ***"
@@ -38,7 +32,6 @@ fi
 
 mkdir -p m4
 
-gtkdocize || exit $?
 autoreconf --force --install --verbose || exit $?
 
 cd "$olddir"
